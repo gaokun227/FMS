ead6c45e9e73372e7f6ab0024c8a425f5685f89c
Kun Gao <Kun.Gao@gaea12.ncrc.gov>
2023-01-18 13:50:33 -0500

    no checksum

--- fms2_io/fms_netcdf_domain_io.F90
+++ fms2_io/fms_netcdf_domain_io.F90
@@ -671,11 +671,11 @@ subroutine restore_domain_state(fileobj, unlim_dim_level)
           is_decomposed) then
         call get_variable_attribute(fileobj, fileobj%restart_vars(i)%varname, &
                                     "checksum", chksum_in_file(1:len(chksum_in_file)))
-        if (.not. string_compare(trim(adjustl(chksum_in_file)), trim(adjustl(chksum)))) then
-          call error("The checksum in the file:"//trim(fileobj%path)//" and variable:"//trim(fileobj%restart_vars(i)%varname)//&
-                     &" does not match the checksum calculated from the data. file:"//trim(adjustl(chksum_in_file))//&
-                     &" from data:"//trim(adjustl(chksum)))
-        endif
+!        if (.not. string_compare(trim(adjustl(chksum_in_file)), trim(adjustl(chksum)))) then
+!          call error("The checksum in the file:"//trim(fileobj%path)//" and variable:"//trim(fileobj%restart_vars(i)%varname)//&
+!                     &" does not match the checksum calculated from the data. file:"//trim(adjustl(chksum_in_file))//&
+!                     &" from data:"//trim(adjustl(chksum)))
+!        endif
       endif
     elseif (associated(fileobj%restart_vars(i)%data4d)) then
       call domain_read_4d(fileobj, fileobj%restart_vars(i)%varname, &
